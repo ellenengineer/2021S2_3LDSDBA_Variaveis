5e8117b91cf32d59bab7854ee8c751c530a80b55
Ellen Santos <drummerandengineer_ellen@hotmail.com>
2021-08-18 21:15:51 -0300

    Add project files.

---
+++ TesteOperadores/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace TesteOperadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Atribuicao();
+            Aritmetico();
+            AritmeticoAtrReduzida();
+            Incrementais();
+            Relacionais();
+            Logicos();
+            Ternarios();
+
+            Console.ReadLine();
+        }
+        public static void Atribuicao()
+        {
+            String strImpressao = "Hello World";
+            Console.WriteLine("Operador de Atribuicao: " + strImpressao);
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+        }
+
+
+        public static void Aritmetico()
+        {
+            int adicao = 5 + 4;
+            int subtracao = 9 - 2;
+            int multiplicacao = 6 * 3;
+            int divisao = 6 / 2;
+            int modulo = 9 % 2;
+
+            Console.WriteLine("Operador Artimético: ");
+            Console.WriteLine("Adição: " + adicao);
+            Console.WriteLine("Subtração: " + subtracao);
+            Console.WriteLine("Multiplicacao: " + multiplicacao);
+            Console.WriteLine("Divisão: " + divisao);
+            Console.WriteLine("Modulo: " + modulo);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+
+
+        }
+
+
+
+        public static void AritmeticoAtrReduzida()
+        {
+            int Maisigual = 5;
+            Maisigual += 4;
+
+            int MenosIgual = 9;
+            MenosIgual -= 2;
+
+            int multiplicacaoigual = 6;
+            multiplicacaoigual *= 3;
+
+            int divisaoigual = 6;
+            divisaoigual /= 2;
+
+            int moduloigual = 9;
+            moduloigual %= 2;
+
+            Console.WriteLine(" Operador Aritméticos de atribuição reduzida: ");
+            Console.WriteLine("+=: " + Maisigual);
+            Console.WriteLine("-=: " + MenosIgual);
+            Console.WriteLine("*=: " + multiplicacaoigual);
+            Console.WriteLine("/=: " + divisaoigual);
+            Console.WriteLine("%=: " + moduloigual);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+        }
+
+        public static void Incrementais()
+        {
+            int incremental = 10;
+            incremental++;
+
+            int decremental = 20;
+            decremental--;
+
+
+            Console.WriteLine("Operador Incremental ++");
+            Console.WriteLine("++: " + incremental);
+            Console.WriteLine("--: " + decremental);
+
+
+            incremental = 10;
+            ++incremental;
+
+            decremental = 20;
+            --decremental;
+
+
+            Console.WriteLine("++ Operador Incremental");
+            Console.WriteLine("++: " + incremental);
+            Console.WriteLine("--: " + decremental);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+
+        }
+
+
+        public static void Relacionais()
+        {
+            int a = 10;
+            int b = 20;
+            bool igualA = a == b;
+            bool diferenteA = a != b;
+            bool maiorQue = a > b;
+            bool menorQue = a < b;
+            bool maiorIgualA = a >= b;
+            bool menorIgualA = a <= b;
+
+            Console.WriteLine("Operadores relacionais");
+            Console.WriteLine("== :" + igualA);
+            Console.WriteLine("!= :" + diferenteA);
+            Console.WriteLine("> :" + maiorQue);
+            Console.WriteLine("< :" + menorQue);
+            Console.WriteLine(">= :" + maiorIgualA);
+            Console.WriteLine("<= :" + menorIgualA);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+
+        }
+
+
+
+        public static void Logicos()
+        {
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            int d = 40;
+
+            bool and = (a < b) && (c < d);
+            bool or = (a < b) || (c < d);
+            bool not = !(a == b);
+
+            Console.WriteLine("Operadores Lógicos");
+            Console.WriteLine("&& :" + and);
+            Console.WriteLine("|| :" + or);
+            Console.WriteLine("! :" + not);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+
+        }
+
+        public static void Ternarios()
+        {
+            int a = 10;
+            int b = 20;
+            string resultadoTernario = a < b ? "Maior!" : "Menor";
+            Console.WriteLine("Operador Ternário: " + resultadoTernario);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+        }
+
+    }
+}
---
+++ TesteVariaveis/Program.cs
@@ -0,0 +1,183 @@
+﻿
+using System;
+using System.Drawing;
+using System.Text;
+
+namespace TesteVariaveis
+{
+    struct Agenda
+    {
+        public DateTime dtAniversario;
+        public string Nome;
+    }
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            TesteDataStruct();
+
+            TesteClsPessoa();
+
+            TesteConcatString();
+
+            ConversaoImplicita();
+
+            ConversaoExplicita();
+
+            ConversaoClasseAuxiliar();
+
+            Boxing();
+
+            Unboxing();
+
+            TesteVar();
+
+            Console.ReadLine();
+
+        }
+
+        public static void TesteDataStruct()
+        {
+            Agenda ag = new Agenda();
+            ag.Nome = "Ellen";
+            ag.dtAniversario = new DateTime(1986, 01, 28);
+
+            Console.WriteLine("Short Date: " + ag.dtAniversario.ToString("d"));
+
+            Console.WriteLine("Long Date: " + ag.dtAniversario.ToString("D"));
+
+            Console.WriteLine("Full Date: " + ag.dtAniversario.ToString("F"));
+
+            Console.WriteLine(" Year / Month: " + ag.dtAniversario.ToString("y"));
+
+            Console.WriteLine("02 / 10 2021: " + ag.dtAniversario.ToString("“MM ‘/’ dd yyyy"));
+
+            Console.WriteLine("10.02.2021: " + ag.dtAniversario.ToString("dd.MM.yyyy"));
+
+            Console.WriteLine("10.02.2021 07:00 : " + ag.dtAniversario.ToString("MM.dd.yyyy HH: mm"));
+
+            Console.WriteLine(" Wednesday @ 7:00 PM : " + ag.dtAniversario.ToString("dddd @ hh: mm tt", System.Globalization.CultureInfo.InvariantCulture));
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+        }
+
+        public static void TesteClsPessoa()
+        {
+            clsPessoa objPessoa = new clsPessoa();
+        }
+
+        public static void TesteConcatString()
+        {
+            //Usando o operador “+”
+            string FraseMais = "Estamos " + "iniciando " + "mais " + "um " + "semestre.";
+
+            //Usando o método Concat
+            string MetodoConcat = string.Concat("Estamos ", "iniciando ", "mais ", "um ", "semestre.");
+
+            //Usando o método join
+            string MetodoJoin = string.Join("Estamos ", "iniciando ", "mais ", "um ", "semestre.");
+
+            //Usando a classe StringBuilder(System.Text)
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Estamos ");
+            sb.Append("iniciando ");
+            sb.Append("mais ");
+            sb.Append("um ");
+            sb.Append("semestre.");
+
+            string stringBuilder = sb.ToString();
+
+            //Usando o método String.Format
+            string MetodoFormat = string.Format("Estamos ", "iniciando ", "mais ", "um ", "semestre.");
+            string MetodoFormat2 = string.Format("{0} {1} {2} {3} {4}", "Estamos ", "iniciando ", "mais ", "um ", "semestre.");
+
+            //Usando o operador $
+            string OperadorDolar = $"{ "Estamos "} { "iniciando "} { "mais "} { "um "} { "semestre."}";
+
+            Console.WriteLine("+:" + FraseMais);
+            Console.WriteLine("Concat: " + MetodoConcat);
+            Console.WriteLine("Join: " + MetodoJoin);
+            Console.WriteLine("stringBuilder: " + stringBuilder);
+            Console.WriteLine("Fomrat: " + MetodoFormat);
+            Console.WriteLine("Format 2 : " + MetodoFormat2);
+            Console.WriteLine("$ : " + OperadorDolar);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+
+        }
+
+        public static void ConversaoImplicita()
+        {
+            int numero1 = 10;
+            double double1 = numero1;
+
+            Console.WriteLine(double1);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+
+        }
+
+        public static void ConversaoExplicita()
+        {
+            double dbl1 = 10.23;
+            int number1 = (int)dbl1;
+            Console.WriteLine("Conversao Explicita: " + number1);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+
+        }
+
+        public static void ConversaoClasseAuxiliar()
+        {
+            int numero1;
+            numero1 = Convert.ToInt32("356");
+
+            Console.WriteLine("Conversao implicita: " + numero1);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+        }
+
+        public static void Boxing()
+        {
+            int num = 123;
+            object obj = num;
+            Console.WriteLine("Boxing: " + obj);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+
+        }
+
+        public static void Unboxing()
+        {
+            int numero = 123;
+            object obj = numero;
+            int num;
+            num = (int)obj;
+            Console.WriteLine("unboxing: " + num);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+
+        }
+
+        public static void TesteVar()
+        {
+            var num = 10;
+            Console.WriteLine("Var: " + num);
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ");
+        }
+    }
+
+
+
+}
